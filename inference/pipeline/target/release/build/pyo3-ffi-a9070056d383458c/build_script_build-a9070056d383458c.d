e8fc6eb4ce65724129e9312c738bb4ed6e86fb4b
Troy Diaz <troyk1718@gmail.com>
2026-05-14 19:00:22 -0700

    Ignore generated pipeline files

--- inference/pipeline/target/release/build/pyo3-ffi-a9070056d383458c/build_script_build-a9070056d383458c.d
+++
@@ -1,5 +0,0 @@
-/Users/troy/Desktop/XRDrone/inference/pipeline/target/release/build/pyo3-ffi-a9070056d383458c/build_script_build-a9070056d383458c.d: /Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-ffi-0.21.2/build.rs
-
-/Users/troy/Desktop/XRDrone/inference/pipeline/target/release/build/pyo3-ffi-a9070056d383458c/build_script_build-a9070056d383458c: /Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-ffi-0.21.2/build.rs
-
-/Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-ffi-0.21.2/build.rs:
--- inference/pipeline/target/release/deps/smallvec-8d937239d00232c2.d
+++
@@ -1,7 +0,0 @@
-/Users/troy/Desktop/XRDrone/inference/pipeline/target/release/deps/smallvec-8d937239d00232c2.d: /Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
-
-/Users/troy/Desktop/XRDrone/inference/pipeline/target/release/deps/libsmallvec-8d937239d00232c2.rlib: /Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
-
-/Users/troy/Desktop/XRDrone/inference/pipeline/target/release/deps/libsmallvec-8d937239d00232c2.rmeta: /Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs
-
-/Users/troy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/smallvec-1.15.1/src/lib.rs:
